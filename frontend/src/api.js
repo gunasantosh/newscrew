1339c3b2a8f947661ccadd9935842e892e0cf7d8
gunasantosh <gunasantosh999@gmail.com>
2025-03-12 11:00:26 +0530

    api url changed

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,13 +1,18 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://127.0.0.1:8000/"; // Replace with your backend URL
+// const API_BASE_URL = "http://127.0.0.1:8000/"; // Replace with your backend URL
+const apiUrl = "/choreo-apis/newscrew/backend/v1";
 
 const api = axios.create({
-  baseURL: API_BASE_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
 });
+
+// const api = axios.create({
+//   baseURL: API_BASE_URL,
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
 
 // Attach token if available
 api.interceptors.request.use((config) => {
